71cbf6f184311d883ea96018b64fedf1f3b943de
SPPSoftware <sppsoftware@gmail.com>
2016-02-14 02:07:55 +0200

    Long time huh.. Login with adminuser and bootstrap

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+  layout 'login_layout'
+  def new
+  end
+
+  def create
+    @user = User.find_by_username(params[:session][:username])
+      if @user && @user.authenticate(params[:session][:password])
+        session[:user_id] = @user.id
+        redirect_to '/'
+      else
+        render action: 'new', alert: "Väärät tunnukset"
+
+      end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to '/'
+  end
+
+end
